ee22a7cfdbca7eb051f79cc2bf2de8da24d77bf6
rons-git <ron@cancunron.com>
2019-02-16 00:16:37 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -85,11 +85,12 @@ int main() {
           meas_package_R.timestamp_ = timestamp_R;
     			ukf.ProcessMeasurement(meas_package_R);
 					if (target_x == 0.0 && target_y == 0.0) {
-						target_x = ukf.x_[0] + (ukf.x_[0] - target_x) * 5;
-						target_y = ukf.x_[1] + (ukf.x_[1] - target_y) * 5;
+						target_x = ukf.x_[0] + (target_x - ukf.x_[0]) * 5;
+						target_y = ukf.x_[1] + (target_y - ukf.x_[1]) * 5;
 					}
 					else {
 						target_x = ukf.x_[0];
+						target_y = ukf.x_[1];
 					}
     			double heading_to_target = atan2(target_y - hunter_y, target_x - hunter_x);
     			while (heading_to_target > M_PI) heading_to_target -= 2. * M_PI; 
