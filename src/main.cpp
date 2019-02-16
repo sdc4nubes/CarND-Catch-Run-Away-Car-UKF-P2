bfd122a4770c561aeac08c2fb187c72244851f84
rons-git <ron@cancunron.com>
2019-02-16 01:27:31 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -89,10 +89,10 @@ int main() {
 					if (target_x != 0.0 && target_y != 0.0) {
 						double diff_x = (save_x - target_x) * 50.;
 						double diff_y = (save_y - target_y) * 50.;
-						target_x = max(target_x + diff_x, 2. * diff_x / fabs(diff_x));
-						target_y += max(target_y + diff_y, 2. * diff_y / fabs(diff_y));
-						if (diff_x < 0.) target_x = min(target_x + diff_x, 2. * diff_x / fabs(diff_x));
-						if (diff_y < 0.) target_y = min(target_y + diff_y, 2. * diff_y / fabs(diff_y));
+						if (diff_x >= 0.) target_x += max(target_x + diff_x, 2. * diff_x / fabs(diff_x));
+						if (diff_x >= 0.) target_y += max(target_y + diff_y, 2. * diff_y / fabs(diff_y));
+						if (diff_x < 0.) target_x += min(target_x + diff_x, 2. * diff_x / fabs(diff_x));
+						if (diff_y < 0.) target_y += min(target_y + diff_y, 2. * diff_y / fabs(diff_y));
 					} else {
 						target_x = save_x;
 						target_y = save_y;
