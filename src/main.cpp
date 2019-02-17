0d7eeab60d5aae6f578ed438cddee674dda42694
rons-git <ron@cancunron.com>
2019-02-17 03:49:06 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -96,6 +96,7 @@ int main() {
 					while (heading_difference > M_PI) heading_difference -= 2.* M_PI;
 					while (heading_difference < -M_PI) heading_difference += 2. * M_PI;
 					heading_difference *= .5;
+					if (distance_difference < .3) heading_difference *= .2;
           json msgJson;
           msgJson["turn"] = heading_difference;
           msgJson["dist"] = distance_difference; 
