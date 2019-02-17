31d4992a7ded1a8d9aab6146f70280ea19fd0f09
rons-git <ron@cancunron.com>
2019-02-17 02:14:54 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -91,9 +91,8 @@ int main() {
 					if (target_y.size() > 10) target_y.pop_back();
 					double avg_x = accumulate(target_x.begin(), target_x.end(), 0.0) / target_x.size();
 					double avg_y = accumulate(target_y.begin(), target_y.end(), 0.0) / target_y.size();
-					/*
 					if (target_x.size() > 2) {
-						size_t size;
+						size_t size = target_x.size();
 						vector<double> temp = target_x;
 						sort(temp.begin(), temp.end());
 						if (size % 2 == 0) avg_x = (temp[size / 2 - 1] + temp[size / 2]) / 2;
@@ -103,7 +102,6 @@ int main() {
 						if (size % 2 == 0) avg_y = (temp[size / 2 - 1] + temp[size / 2]) / 2;
 						else avg_y = temp[size / 2];
 					}
-					*/
 					double distance_difference = sqrt((avg_y - hunter_y) * (avg_y - hunter_y) + \
 						(avg_x - hunter_x) * (avg_x - hunter_x));
 					double heading_to_target = 1. / -atan2(avg_y - hunter_y, avg_x - hunter_x);
