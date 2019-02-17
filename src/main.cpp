db924c4a541eb569c7712f18975c064f0438aa8e
rons-git <ron@cancunron.com>
2019-02-16 20:24:59 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -87,7 +87,7 @@ int main() {
 					//cout << "h_x:, " << hunter_x << ", h_y:, " << hunter_y << ", t_x:, " << target_x << ", t_y:, " << target_y << ", p_x:, " << ukf.x_[0] << ", p_y:, " << ukf.x_[1] << endl;
 					target_x = ukf.x_[0];
 					target_y = ukf.x_[1];
-    			double heading_to_target = atan2(target_y * 1.5 - hunter_y, target_x * 1.5 - hunter_x);
+    			double heading_to_target = atan2(target_y * 20 - hunter_y, target_x * 20 - hunter_x);
     			while (heading_to_target > M_PI) heading_to_target -= 2. * M_PI; 
     			while (heading_to_target < -M_PI) heading_to_target += 2. * M_PI;
     			//turn towards the target
@@ -96,11 +96,9 @@ int main() {
     			while (heading_difference < -M_PI) heading_difference += 2. * M_PI;
     			double distance_difference = sqrt((target_y - hunter_y) * (target_y - hunter_y) + \
 						(target_x - hunter_x) * (target_x - hunter_x));
-					hunter_heading += heading_difference;
           json msgJson;
 					cout << hunter_heading << heading_difference << endl;
-          //msgJson["turn"] = heading_difference;
-					msgJson["turn"] = hunter_heading;
+          msgJson["turn"] = heading_difference;
           msgJson["dist"] = distance_difference; 
           auto msg = "42[\"move_hunter\"," + msgJson.dump() + "]";
           // std::cout << msg << std::endl;
