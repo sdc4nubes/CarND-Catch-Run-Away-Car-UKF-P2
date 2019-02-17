f8a21b1d79feede28b45644450d075a6f27e433b
rons-git <ron@cancunron.com>
2019-02-17 06:55:12 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -90,7 +90,7 @@ int main() {
 					double distance_difference = sqrt((target_y - hunter_y) * (target_y - hunter_y) + \
 						(target_x - hunter_x) * (target_x - hunter_x));
 					if (distance_difference > 15.) go_home = true;
-					if (target_x <= -9.) go_home = false;
+					if (distance_difference < 3.) go_home = false;
 					double heading_to_target = 1. / -atan2(target_y - hunter_y, target_x - hunter_x);
 					if (go_home) heading_to_target = atan2(target_y - hunter_y, target_x - hunter_x);
 					while (heading_to_target > M_PI) heading_to_target -= 2. * M_PI;
