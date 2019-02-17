6227bf77bd4cd54134533b65306fb6ecc213253c
rons-git <ron@cancunron.com>
2019-02-17 04:40:54 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -87,14 +87,10 @@ int main() {
     			ukf.ProcessMeasurement(meas_package_R);
 					target_x = ukf.x_[0];
 					target_y = ukf.x_[1];
-					if (go_home) {
-						target_x = 0.0;
-						target_y = 0.0;
-						if (fabs(hunter_y) < .1 && fabs(hunter_x) < .1) go_home = false;
-					}
 					double distance_difference = sqrt((target_y - hunter_y) * (target_y - hunter_y) + \
 						(target_x - hunter_x) * (target_x - hunter_x));
 					if (distance_difference > 20.) go_home = true;
+					if (go_home && distance_difference < 10.) go_home = false;
 					double heading_to_target = 1. / -atan2(target_y - hunter_y, target_x - hunter_x);
 					if (go_home) heading_to_target = atan2(target_y - hunter_y, target_x - hunter_x);
 					while (heading_to_target > M_PI) heading_to_target -= 2. * M_PI;
