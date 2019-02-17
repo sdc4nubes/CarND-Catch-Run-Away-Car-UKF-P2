d45b384fd7d884448fdc7bb4b0f4302bf80c5a7b
rons-git <ron@cancunron.com>
2019-02-16 23:35:37 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -86,13 +86,11 @@ int main() {
     			ukf.ProcessMeasurement(meas_package_R);
 					double save_target_x = target_x;
 					double save_target_y = target_y;
-					target_x = .01 * ukf.x_[0] + .99 * save_target_x;
-					target_y = .01 * ukf.x_[1] + .99 * save_target_y;
+					target_x = .1 * ukf.x_[0] + .9 * save_target_x;
+					target_y = .1 * ukf.x_[1] + .9 * save_target_y;
 					double distance_difference = sqrt((target_y - hunter_y) * (target_y - hunter_y) + \
 						(target_x - hunter_x) * (target_x - hunter_x));
 					double heading_to_target = 1. / -atan2(target_y - hunter_y, target_x - hunter_x);
-					if (distance_difference < .5) 
-						heading_to_target = -atan2(target_y - hunter_y, target_x - hunter_x);
 					while (heading_to_target > M_PI) heading_to_target -= 2. * M_PI;
 					while (heading_to_target < -M_PI) heading_to_target += 2. * M_PI;
 					//turn towards the target
