fcbe4a44e62dc9deb275a07f4c7031a2b0565d6d
rons-git <ron@cancunron.com>
2019-02-17 04:33:42 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -105,6 +105,8 @@ int main() {
 					while (heading_difference < -M_PI) heading_difference += 2. * M_PI;
 					if (not go_home) heading_difference *= .5;
 					if (distance_difference < .3) heading_difference *= .2;
+					if (heading_difference > 0.) heading_difference = min(heading_difference, .1);
+					else heading_difference = max(heading_difference, -.1);
           json msgJson;
           msgJson["turn"] = heading_difference;
           msgJson["dist"] = distance_difference; 
