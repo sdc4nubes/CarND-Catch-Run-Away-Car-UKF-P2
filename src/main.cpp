2cf5073db56989c7b7778d12ae627b85a149b6a6
rons-git <ron@cancunron.com>
2019-02-17 17:47:43 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -30,9 +30,8 @@ int main() {
   UKF ukf;
   double target_x = 0.;
 	double target_y = 0.;
-	double min_distance = 99.;
 	int go_home = false;
-  h.onMessage([&ukf, &target_x, &target_y, &go_home, &min_distance]
+  h.onMessage([&ukf, &target_x, &target_y, &go_home]
 		(uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -86,19 +85,14 @@ int main() {
           iss_R >> timestamp_R;
           meas_package_R.timestamp_ = timestamp_R;
     			ukf.ProcessMeasurement(meas_package_R);
-					if (hunter_x == -10.) {
-						go_home = 0;
-						min_distance = 99.;
-					}
+					if (hunter_x == -10.) go_home = 0;
 					target_x = ukf.x_[0];
 					target_y = ukf.x_[1];
 					double distance_difference = sqrt((target_y - hunter_y) * (target_y - hunter_y) + \
 						(target_x - hunter_x) * (target_x - hunter_x));
-					if (distance_difference < min_distance) min_distance = distance_difference;
-					if (distance_difference > 6 && min_distance < 3.) go_home = 1;
 					if (distance_difference > 12.) go_home = 1;
-					if (distance_difference < 3. && go_home == 1) go_home = 0;
-					double heading_to_target = .5 / -atan2(target_y - hunter_y, target_x - hunter_x);
+					if (distance_difference < 3.) go_home = 0;
+					double heading_to_target = .75 / -atan2(target_y - hunter_y, target_x - hunter_x);
 					if (go_home > 0) heading_to_target = atan2(target_y - hunter_y, target_x - hunter_x);
 					while (heading_to_target > M_PI) heading_to_target -= 2. * M_PI;
 					while (heading_to_target < -M_PI) heading_to_target += 2. * M_PI;
