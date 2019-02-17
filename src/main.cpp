8c29f10c323b2ffa91db88d1964d15b73e20ab1d
rons-git <ron@cancunron.com>
2019-02-16 21:23:48 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -92,7 +92,6 @@ int main() {
 					double distance_difference;
 					double heading_to_target = atan2(target_y - hunter_y, target_x - hunter_x);
 					while (iflag < 2) {
-						double heading_to_target = atan2(target_y - hunter_y, target_x - hunter_x);
 						while (heading_to_target > M_PI) heading_to_target -= 2. * M_PI;
 						while (heading_to_target < -M_PI) heading_to_target += 2. * M_PI;
 						//turn towards the target
