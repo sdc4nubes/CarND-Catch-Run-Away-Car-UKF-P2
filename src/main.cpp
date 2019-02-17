7063a70a00e768f8e82bf8ac265ee48138e68433
rons-git <ron@cancunron.com>
2019-02-16 21:02:32 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -87,15 +87,13 @@ int main() {
 					//cout << "h_x:, " << hunter_x << ", h_y:, " << hunter_y << ", t_x:, " << target_x << ", t_y:, " << target_y << ", p_x:, " << ukf.x_[0] << ", p_y:, " << ukf.x_[1] << endl;
 					target_x = ukf.x_[0];
 					target_y = ukf.x_[1];
-					double adj_x = target_x * 10;
-					double adj_y = target_y * 10;
-    			double heading_to_target = atan2(adj_y - hunter_y, adj_x - hunter_x);
-    			//while (heading_to_target > M_PI) heading_to_target -= 2. * M_PI; 
-    			//while (heading_to_target < -M_PI) heading_to_target += 2. * M_PI;
+    			double heading_to_target = 1 / -atan2(target_y - hunter_y, target_x - hunter_x);
+    			while (heading_to_target > M_PI) heading_to_target -= 2. * M_PI; 
+    			while (heading_to_target < -M_PI) heading_to_target += 2. * M_PI;
     			//turn towards the target
     			double heading_difference = heading_to_target - hunter_heading;
-    			//while (heading_difference > M_PI) heading_difference -= 2.* M_PI; 
-    			//while (heading_difference < -M_PI) heading_difference += 2. * M_PI;
+    			while (heading_difference > M_PI) heading_difference -= 2.* M_PI; 
+    			while (heading_difference < -M_PI) heading_difference += 2. * M_PI;
     			double distance_difference = sqrt((target_y - hunter_y) * (target_y - hunter_y) + \
 						(target_x - hunter_x) * (target_x - hunter_x));
           json msgJson;
