cd7ab2b07c114f6c39973bbe718d4b29926974a6
rons-git <ron@cancunron.com>
2019-02-17 03:07:05 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -94,7 +94,7 @@ int main() {
 					double distance_difference = sqrt((avg_y - hunter_y) * (avg_y - hunter_y) + \
 						(avg_x - hunter_x) * (avg_x - hunter_x));
 					double heading_to_target = 1. / -atan2(avg_y - hunter_y, avg_x - hunter_x);
-					if (distance_difference < .4) {
+					if (distance_difference < .5) {
 						avg_x = target_x.back();
 						avg_y = target_y.back();
 						heading_to_target = atan2(avg_y - hunter_y, avg_x - hunter_x);
