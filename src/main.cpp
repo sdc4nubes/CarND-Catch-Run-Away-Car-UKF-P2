864b7818fba48ab827cefc390695def58cdc7af1
rons-git <ron@cancunron.com>
2019-02-16 21:41:21 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -100,7 +100,7 @@ int main() {
 						while (heading_difference < -M_PI) heading_difference += 2. * M_PI;
 						distance_difference = sqrt((target_y - hunter_y) * (target_y - hunter_y) + \
 							(target_x - hunter_x) * (target_x - hunter_x));
-						if (distance_difference > 1. && iflag == 0) {
+						if (iflag == 0) {
 							heading_to_target = 1 / -atan2(target_y - hunter_y, target_x - hunter_x);
 							iflag += 1;
 						}
