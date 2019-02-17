6cfb9f65f702dd4d7ce4d800c64cb3620379e1c3
rons-git <ron@cancunron.com>
2019-02-17 06:22:53 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -92,14 +92,14 @@ int main() {
 					if (distance_difference > 15.) go_home = true;
 					if (distance_difference < 5.) go_home = false;
 					double heading_to_target = 1. / -atan2(target_y - hunter_y, target_x - hunter_x);
-					if (go_home) heading_to_target = atan2(-hunter_y, -hunter_x);
+					if (go_home) heading_to_target = atan2(target_y - hunter_y, target_x - hunter_x);
 					while (heading_to_target > M_PI) heading_to_target -= 2. * M_PI;
 					while (heading_to_target < -M_PI) heading_to_target += 2. * M_PI;
 					//turn towards the target
 					double heading_difference = heading_to_target - hunter_heading;
 					while (heading_difference > M_PI) heading_difference -= 2.* M_PI;
 					while (heading_difference < -M_PI) heading_difference += 2. * M_PI;
-					heading_difference *= .1;
+					heading_difference *= .5;
           json msgJson;
           msgJson["turn"] = heading_difference;
           msgJson["dist"] = distance_difference; 
