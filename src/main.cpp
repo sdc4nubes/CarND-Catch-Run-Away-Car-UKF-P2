8d94d07505198733683ee197832d4b849cd5fe24
rons-git <ron@cancunron.com>
2019-02-16 20:31:08 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -87,15 +87,17 @@ int main() {
 					//cout << "h_x:, " << hunter_x << ", h_y:, " << hunter_y << ", t_x:, " << target_x << ", t_y:, " << target_y << ", p_x:, " << ukf.x_[0] << ", p_y:, " << ukf.x_[1] << endl;
 					target_x = ukf.x_[0];
 					target_y = ukf.x_[1];
-    			double heading_to_target = atan2(target_y * 20 - hunter_y, target_x * 20 - hunter_x);
+					double adj_x = target_x * 20;
+					double adj_y = target_y * 20;
+    			double heading_to_target = atan2(adj_y - hunter_y, adj_x - hunter_x);
     			while (heading_to_target > M_PI) heading_to_target -= 2. * M_PI; 
     			while (heading_to_target < -M_PI) heading_to_target += 2. * M_PI;
     			//turn towards the target
     			double heading_difference = heading_to_target - hunter_heading;
     			while (heading_difference > M_PI) heading_difference -= 2.* M_PI; 
     			while (heading_difference < -M_PI) heading_difference += 2. * M_PI;
-    			double distance_difference = sqrt((target_y - hunter_y) * (target_y - hunter_y) + \
-						(target_x - hunter_x) * (target_x - hunter_x));
+    			double distance_difference = sqrt((adj_y - hunter_y) * (adj_y - hunter_y) + \
+						(adj_x - hunter_x) * (adj_x - hunter_x));
           json msgJson;
 					cout << hunter_heading << heading_difference << endl;
           msgJson["turn"] = heading_difference;
