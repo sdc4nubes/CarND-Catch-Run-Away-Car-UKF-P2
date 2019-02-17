1dfb263c3253b65e42105dae6dff584384c45182
rons-git <ron@cancunron.com>
2019-02-17 16:46:07 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -97,7 +97,7 @@ int main() {
 					if (distance_difference < min_distance) min_distance = distance_difference;
 					if ((distance_difference > 9. && min_distance < 3.) || distance_difference > 12.)
 						go_home = true;
-					if (distance_difference < 3.) go_home = false;
+					//if (distance_difference < 3.) go_home = false;
 					double num = .75;
 					if (min_distance < 3.) num = .5;
 					double heading_to_target = num / -atan2(target_y - hunter_y, target_x - hunter_x);
